6ec063a7091e928bc9c4588ddd21eda1fbf68a0f
Jeff Maughan <geophray@gmail.com>
2020-02-14 15:47:18 -0700

    Fix each in part 1

--- src/underbar.js
+++ src/underbar.js
@@ -53,6 +53,15 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(collection.toString() === "[object Object]") {
+      for (var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    } else {
+      for( let i = 0; i < collection.length; i++ ) {
+        iterator(collection[i], i, collection);
+      }
+    }  
   };
 
   // Returns the index at which value can be found in the array, or -1 if value
